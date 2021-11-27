79448d7f2c667eb1be8806824b512019f6cfbad9
你好2007 <2501482523@qq.com>
2021-11-27 13:35:32 +0800

    “提供常用的DOM操作方法” xhtml.js + “获取键盘此时按下的键的组合结果” getKeyString.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import formatColor from "./formatColor";
+import xhtml from './xhtml';
+import getKeyString from './getKeyString';
 
 // 导出
 var browser = {
 
     // 颜色格式化
-    formatColor: formatColor
+    formatColor: formatColor,
+
+    // DOM操作
+    xhtml: xhtml,
+
+    // 键盘按键
+    getKeyString: getKeyString
 
 };
 
