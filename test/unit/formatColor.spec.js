fb774e916591223d04deff7c8ccae52cc500ce25
你好2007 <2501482523@qq.com>
2022-02-23 17:45:32 +0800

    单元测试框架改为： https://www.npmjs.com/package/async-allege-web

--- test/unit/formatColor.spec.js
+++ test/unit/formatColor.spec.js
@@ -1,5 +1,10 @@
-QUnit.test('颜色格式化', 1, function () {
+allege.test('颜色格式化', function (handler) {
 
-    deepEqual([255, 0, 0, 1], browser.formatColor('red'));
+    handler.do(function () {
+
+        var red = browser.formatColor('red');
+        return red[0] == 255 && red[1] == 0 && red[2] == 0 && red[3] == 1
+
+    }, 'red = rgba(255,0,0,1)');
 
 });
